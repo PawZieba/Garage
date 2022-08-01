e8f4b8fd84405082a11e9b60a5d5d720df6f02ec
Paweł Zięba <pzieba@o2.pl>
2022-08-01 13:40:23 +0200

    add dictionary

---
+++ Garage/Models/Dictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage.Models
+{
+    public class Dictionary
+    {
+        public const string firstname = "Imię";
+        public const string lastname = "Nazwisko";
+        public const string phone = "Telefon";
+        public const string producer = "Marka";
+        public const string model = "Model";
+        public const string vin = "VIN";
+        public const string licencePlate = "Numer rejestracyjny";
+        public const string owner = "Właściciel";
+        public const string create = "Utwórz";
+        public const string back = "Powrót";
+        public const string delete = "Usuń";
+        public const string save = "Zapisz";
+        public const string edit = "Edycja";
+        public const string ofcustomer = "Klienta";
+        public const string ofcar = "samochodu";
+        public const string car = "samochód";
+        public const string cars = "Samochody";
+        public const string lackOfAssignedCars = "Brak przypisanych samochodów";
+        public const string addCustomer = "Dodaj Klienta";
+        public const string addCar = "Dodaj samochód";
+    }
+}
