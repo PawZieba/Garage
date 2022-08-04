146da76dfc7cb7626d3069c9d65bde05d626d23b
Paweł Zięba <pzieba@o2.pl>
2022-08-04 21:14:18 +0200

    change date format for repair date

--- Garage/Models/Repair.cs
+++ Garage/Models/Repair.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Garage.Models
     public class Repair
     {
         public int ID { get; set; }
+        [DisplayFormat(DataFormatString = "{0:d}")] 
         public DateTime RepairDate { get; set; }
         public int MileAge { get; set; }
         public string Description { get; set; }
